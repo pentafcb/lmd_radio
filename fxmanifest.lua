5c1d7abe9576535a80898e28ba6b9622e76d7246
𝙋𝙚𝙣𝙩𝙖𝙘𝙖𝙢𝙥𝙚ó𝙣 <68836355+pentafcb@users.noreply.github.com>
2025-02-07 14:09:08 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "cerulean"
 game "gta5"
 author 'LMD Group'
-description 'Garage System https://lmdgroup.tebex.io'
+description 'Police Radio System https://lmdgroup.tebex.io'
 lua54 'on'
 shared_scripts {
     '@ox_lib/init.lua',
